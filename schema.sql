d053b27f55831f169da557c190fe13c5f16ce15b
Alison Lubar <aslubar@gmail.com>
2020-11-13 18:37:03 -0500

    added tables to schema SQL file

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE database employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR (30),
+)
+
+-- does INT need NULL? Why?
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR (30),
+    salary DECIMAL,
+    department_id INT,
+)
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT,
+    manager_id INT,
+)
