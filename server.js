364154654378d337ae4d8b871b6da76d2896e121
Alison Lubar <aslubar@gmail.com>
2020-11-13 18:41:09 -0500

    added initial start function and inquirer prompt to server JS file

--- server.js
+++ server.js
@@ -27,3 +27,21 @@ connection.connect(function (err) {
     // run the start function after the connection is made to prompt the user
     start();
 });
+
+function start() {
+    inquirer
+        .prompt({
+            name: "initialPrompt",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees.",
+                "View All Employees by Department.",
+                "View All Employees by Manager.",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+            ]
+        });
+}
